87aa7cb51973ccb3a7a3ef900c02e60cfa7e149b
santiagota <santiagota@T25005>
2025-10-01 16:44:08 +0200

    .

---
+++ Scheduller/Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+internal class DateController : ControllerBase
+{
+    [HttpPost("calcular")]
+    public IActionResult Calcular([FromBody] FechaRequest request)
+    {
+
+    }
+}
--- Scheduller/Program.cs
+++ Scheduller/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿using System;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
+
+
+public class ProximaFecha
+{
+    public static DateTimeOffset ObtenProximaFecha(FechaSolicitada FechaInicial)
+    {
+        if (FechaInicial.Fecha >= FechaInicial.FechaInicio && FechaInicial.Fecha <= FechaInicial.FechaFin)
+        {
+            return FechaInicial.Periodicidad switch { 
+                Periodicidad.Unico => FechaInicial.Fecha.AddDays(1),
+                Periodicidad.Recurrente => FechaInicial.Fecha.AddDays(3)
+            };
+        }
+        else
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(ObtenProximaFecha(new FechaSolicitada()));
+    }
+}
